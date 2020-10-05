b8bd2b7a95205e9e1c5af8881619845399942637
EricShi1685 <66964762+EricShi1685@users.noreply.github.com>
2020-10-04 18:05:59 -0700

    Add files via upload

--- mootube.java
+++ mootube.java
@@ -1,55 +1,52 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
-public class App {
-    static Scanner in = new Scanner(System.in);
-    static int n = in.nextInt();
-    static int q = in.nextInt();
-    static List<ArrayList<connection>> adjList = new ArrayList<ArrayList<connection>>(); 
-    static boolean[] visited = new boolean[n + 1];
-    static int nodeCnt = 0;
-
-    public static void DFS(int node, int relevance){
+import java.io.*;
+import java.util.*;
+public class mootube {
+    static int n, q;
+    static List<ArrayList<edge>> adjList = new ArrayList<>();
+    static boolean[] visited;
+    static int cnt = 0;
+    static void dfs(int node, int k){
         visited[node] = true;
-        ArrayList<connection> adj = adjList.get(node);
-        for(int i = 0; i < adj.size(); i++){
-            connection nNode = adj.get(i);
-            if(nNode.relevance >= relevance && visited[nNode.ID] == false){
-                nodeCnt++;
-                DFS(nNode.ID, relevance);
+        cnt++;
+        for(edge i: adjList.get(node)){
+            if(!visited[i.b] && i.e >= k){
+                dfs(i.b, k);
             }
         }
-        return;
     }
-
-    public static void main(String[] args) throws Exception {
-        adjList.add(new ArrayList<connection>());
-        for(int i = 0; i < n; i++){
-            adjList.add(new ArrayList<connection>());
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new FileReader("mootube.in"));
+        PrintWriter out = new PrintWriter(new File("mootube.out"));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+        visited = new boolean[n + 1];
+        for(int i = 0; i <= n; i++){
+            adjList.add(new ArrayList<>());
         }
         for(int i = 0; i < n - 1; i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int relevance = in.nextInt();
-            adjList.get(a).add(new connection(b, relevance));
-            adjList.get(b).add(new connection(a, relevance));
+            st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            adjList.get(a).add(new edge(b, e));
+            adjList.get(b).add(new edge(a, e));
         }
         for(int i = 0; i < q; i++){
-            int k = in.nextInt();
-            int st = in.nextInt();
-            DFS(st, k);
-            System.out.println(nodeCnt);
-            nodeCnt = 0;
             visited = new boolean[n + 1];
+            cnt = 0;
+            st = new StringTokenizer(in.readLine());
+            int k = Integer.parseInt(st.nextToken()), v = Integer.parseInt(st.nextToken());
+            dfs(v, k);
+            out.println(cnt - 1);
         }
+        in.close();
+        out.close();
     }
 }
-
-class connection{
-    int ID;
-    int relevance;
-    public connection(int ID, int relevance){
-        this.ID = ID;
-        this.relevance = relevance;
+class edge{
+    int b, e;
+    public edge(int n, int e){
+        this.b = n;
+        this.e = e;
     }
 }
