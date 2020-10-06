17bc1c904ef4341427ea925e3b6c745bae331a72
EricShi1685 <66964762+EricShi1685@users.noreply.github.com>
2020-10-06 13:52:04 -0700

    Add files via upload

--- socdist.java
+++ socdist.java
@@ -1,87 +1,65 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class socdist {
-    public static void main(String[] args)throws Exception{
-        FileReader reader = new FileReader("socdist.in");
-        BufferedReader in = new BufferedReader(reader);
+    static int n, m;
+    static List<grass> fields = new ArrayList<>();
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new FileReader("socdist.in"));
+        PrintWriter out = new PrintWriter(new File("socdist.out"));
         StringTokenizer st = new StringTokenizer(in.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        List<patch> patches = new ArrayList<patch>();
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(in.readLine());
-            patches.add(new patch(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+            long a = Long.parseLong(st.nextToken()), b = Long.parseLong(st.nextToken());
+            fields.add(new grass(a, b));
         }
         in.close();
-        Collections.sort(patches, new sort());
-        int best = -1;
-        int lb = 0;
-        int ub = 900000000;
-        while(lb != ub){
-            int guess = (lb + ub)/2;
-            if(check(guess, patches, n, m)){
-                lb = guess + 1;
-                if(guess > best){
-                    best = guess;
-                }
+        fields.sort(new sort());
+        out.println(solve());
+        out.close();
+    }
+    static int solve(){
+        int ans = -1, l = 0, r = Integer.MAX_VALUE;
+        while(l <= r){
+            int m = (l + r)/2;
+            if(valid(m)){
+                l = m + 1;
+                ans = Math.max(m, ans);
             }
             else{
-                ub = guess;
+                r = m - 1;
             }
         }
-        File out = new File("socdist.out");
-        FileWriter writer = new FileWriter(out);
-        writer.write(Integer.toString(best));
-        writer.close();
+        return ans;
     }
-    public static boolean check(int guess, List<patch> patches, int n, int m){
-        long tot = 0;
-        long lastCowPos = patches.get(0).st - guess;
-        for(int i = 0; i < m; i++){
-            if(tot >= n){
-                return true;
-            }
-            patch curr = patches.get(i);
-            long st1 = 0;
-            if(lastCowPos + guess > curr.en){
-                continue;
-            }
-            if(lastCowPos + guess <= curr.st){
-                st1 = curr.st;
-            }
-            else{
-                st1 = lastCowPos + guess;
-            }
-            long numCows = (curr.en - st1)/guess + 1;
-            tot = tot + numCows;
-            lastCowPos = st1 + ((numCows - 1) * guess);
+    static boolean valid(int m){
+        long st = fields.get(0).l;
+        long cnt = 0;
+        for(grass i: fields){
+            st = Math.max(i.l, st);
+            long add = (st <= i.r) ? (i.r - st)/m + 1 : 0;
+            cnt += add;
+            st += m * add;
         }
-        if(tot >= n){
-            return true;
-        }
-        return false;
+        return cnt >= n;
     }
 }
-
-class patch{
-    long st;
-    long en;
-    public patch(long st, long en){
-        this.st = st;
-        this.en = en;
+class grass{
+    long l, r;
+    public grass(long a, long b){
+        this.l = a;
+        this.r = b;
     }
 }
-
-class sort implements Comparator<patch>{
-    public int compare(patch a, patch b){
-        return (int)(a.st - b.st);
+class sort implements Comparator<grass>{
+    public int compare(grass a, grass b){
+        if(a.l < b.l){
+            return -1;
+        }
+        else if(a.l > b.l){
+            return 1;
+        }
+        return 0;
     }
 }
