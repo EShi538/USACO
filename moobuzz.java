d7b988e2845de4ec23f5561893e12e24b147748a
EricShi1685 <66964762+EricShi1685@users.noreply.github.com>
2020-10-25 11:58:28 -0700

    Add files via upload

--- moobuzz.java
+++ moobuzz.java
@@ -1,62 +1,67 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-
-public class moobuzz {
-    public static void main(String[] args) throws Exception {
-        FileReader reader = new FileReader("moobuzz.in");
-        BufferedReader in = new BufferedReader(reader);
-        int n = Integer.parseInt(in.readLine());
+import java.io.*;
+public class moobuzz{
+    static int n;
+    public static void main(String[] args) throws Exception{
+        BufferedReader in = new BufferedReader(new FileReader("moobuzz.in"));
+        PrintWriter out = new PrintWriter(new File("moobuzz.out"));
+        n = Integer.parseInt(in.readLine());
         in.close();
-
-        long lb = 0;
-        long ub = Integer.MAX_VALUE;
-        while(lb != ub){
-            long m = (lb + ub)/2;
-            if(check(m, n) == -2){
-                m--;
-            }
-            if(check(m, n) == 1){
-                ub = m - 1;
-            }
-            else if(check(m, n) == -1){
-                lb = m + 1;
-            }
-            else{
-                if(m % 5 == 0 || m % 3 == 0){
-                    if(m % 15 == 10 || m % 15 == 6){
-                        ub = m - 2;
-                        lb = m - 2;
-                    }
-                    else {
-                        ub = m - 1;
-                        lb = m - 1;
-                    }
+        int ans = (int)solve();
+        switch(ans % 15){
+            case 3:
+            case 0:
+            case 12: {
+                if(check(ans - 1) == n){
+                    ans--;
                 }
                 else{
-                    ub = m;
-                    lb = m;
+                    ans++;
                 }
+                break;
+            }
+            case 5:
+            case 9: {
+                if(check(ans - 1) == n){
+                    ans--;
+                }
+                else{
+                    ans += 2;
+                }
+                break;
+            }
+            case 6:
+            case 10: {
+                if(check(ans - 2) == n){
+                    ans -= 2;
+                }
+                else{
+                    ans++;
+                }
+                break;
             }
         }
-
-        File file = new File("moobuzz.out");
-        FileWriter writer = new FileWriter(file);
-        writer.write(Long.toString(lb));
-        writer.close();
+        out.println(ans);
+        out.close();
     }
-
-    public static int check(long x, int n){
-        long tot = x/3 + x/5 - x/15;
-        if(x - tot > n){
-            return 1;
-        } 
-        else if(x - tot < n){
-            return -1;
+    static long solve(){
+        long l = 0, r = Integer.MAX_VALUE;
+        while(l != r){
+            long m =(int)((long)(l + r)/2);
+            if(check(m) < n){
+                l = m + 1;
+            }
+            else if(check(m) > n){
+                r = m;
+            }
+            else{
+                l = m; r = m;
+            }
         }
-        else{
-            return 0;
-        }
+        return l;
+    }
+    static long check(long m){
+        long div3 = m/3, div5 = m/5, div15 = m/15;
+        long t = (div3 + div5) - div15;
+        return m - t;
     }
 }
