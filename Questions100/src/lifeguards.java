a790a0bb39f1860acfdd96cca9b41c9134ec4050
EricShi1685 <66964762+EricShi1685@users.noreply.github.com>
2021-07-26 21:44:49 -0700

    7/26

---
+++ Questions100/src/lifeguards.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class lifeguards {
+    public static void main(String[] args) throws Exception{
+        FileReader reader = new FileReader("lifeguards.in");
+        BufferedReader in = new BufferedReader(reader);
+        int n = Integer.parseInt(in.readLine());
+        List<time> times = new ArrayList<time>();
+        for(int i = 0; i < n; i++){
+            StringTokenizer st = new StringTokenizer(in.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            times.add(new time(a, (i + 1)));
+            times.add(new time(b, -(i + 1)));
+        }
+        in.close();
+
+        int tot = 0;
+        Collections.sort(times, new sortTime());
+        TreeSet<Integer> onDuty = new TreeSet<Integer>();
+        int[] alone = new int[n];
+        for(int i = 0; i < times.size() - 1; i++){
+            if(times.get(i).type > 0){
+                onDuty.add(times.get(i).type);
+            }
+            else if(times.get(i).type < 0){
+                onDuty.remove(-(times.get(i).type));
+            }
+            
+            int length = times.get(i + 1).point - times.get(i).point;
+            if(onDuty.size() == 1){
+                alone[onDuty.first() - 1] = alone[onDuty.first() - 1] + length;
+            }
+            if(!onDuty.isEmpty()){
+                tot = tot + length;
+            }
+        }
+        int min = Arrays.stream(alone).min().getAsInt();
+        File out = new File("lifeguards.out");
+        FileWriter writer = new FileWriter(out);
+        writer.write(Integer.toString(tot - min));
+        writer.close();
+    }
+}
+
+class time{
+    int point;
+    int type;
+    public time(int point, int type){
+        this.point = point;
+        this.type = type;
+    }
+}
+
+class sortTime implements Comparator<time>{
+    public int compare(time a, time b){
+        return a.point - b.point;
+    }
+}
